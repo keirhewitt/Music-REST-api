d9354f09264b568d30000b89ff22e4ca2b42f028
Keir Hewitt <keir11@hotmail.com>
2023-01-10 20:52:46 +0000

    Feature: added all CRUD options for Users

--- server/controllers/user/users.js
+++ server/controllers/user/users.js
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 import generateApiKey from "generate-api-key";
 import User from "../../models/user/User.js";
 import { sendAPIKey } from "../../services/mail.js";
+import { brotliCompress } from "zlib";
 
 /* CREATE User */
 export const createUser = async (req, res) => {
@@ -26,5 +27,56 @@ export const createUser = async (req, res) => {
         res.status(201).json({ msg: "Your API Key is being sent to your email." });
     } catch (err) {
         res.status(500).json({ error: err.message });
+    }
+}
+
+/* READ User */
+export const readUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = User.findById(id);
+        if (!user) return res.status(404).json({ error: "Cannot find user with id." })
+        res.status(200).json(user); // Return User object
+    } catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+}
+
+/* UPDATE User */
+export const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { email, password } = req.body;
+
+        const updatedDetails = { email: email }; // Property that will be updated
+
+        const user = await User.findById({ _id: id });  // Find User by ID
+        if (!user) return res.status(404).json({ error: "User does not exist." });
+
+        const authPassword = await bcrypt.compare(password, user.password); // Authorise password provided in payload
+        if (!authPassword) return res.status(404).json({ error: "Invalid credentials." });
+
+        const duplicate = await User.find({ email: newemail });  // Make sure new email is not already used by someone else
+        if (duplicate) return res.status(400).json({ error: "This email is already in use." });
+        
+        await User.updateOne(filter, updatedDetails);   // Update User (filter) with new details
+        await user.save();
+
+        res.status(200).json({ message: "User details updated." });
+    } catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+}
+
+/* DELETE User */
+export const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = User.findById(id);
+        if (!user) return res.status(404).json({ error: "Cannot find user with id." })
+        await User.deleteOne({ _id: id });
+        res.status(200).json({ message: "User deleted." });
+    } catch (err) {
+        res.status(404).json({ error: err.message });
     }
 }
