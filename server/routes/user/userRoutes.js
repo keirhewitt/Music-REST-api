1a0ee4bc0d165026ef5e47bf1d3af84d0eeccea1
Keir Hewitt <keir11@hotmail.com>
2023-01-10 20:54:46 +0000

    Update: Updated User routes

--- server/routes/user/userRoutes.js
+++ server/routes/user/userRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { createUser } from "../../controllers/user/users.js";
+import { createUser, readUser, updateUser, deleteUser } from "../../controllers/user/users.js";
+import { isAuthenticated } from "../../middleware/authentication.js";
 
 const router = express.Router();
 
 /* Create user does NOT require authentication (for obvious reasons) */
 router.post('/create', createUser);
+
+/* Delete a User (Authentication required) */
+router.delete('/delete/:userId', isAuthenticated, deleteUser);
+
+/* Update a User (Authentication required) */
+router.patch('/update/:userId', isAuthenticated, updateUser);
+
+/* Get a User (Authentication required) */
+router.get('/:userId', isAuthenticated, readUser);
 
 export default router;
