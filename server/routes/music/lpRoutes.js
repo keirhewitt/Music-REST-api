4546b207ad88bc334b2cf5a745004004f4c0ca78
Keir Hewitt (macbook) <keir11@hotmail.com>
2023-01-11 11:21:17 +0000

    Update: Created new routes for Update and Delete LP

--- server/routes/music/lpRoutes.js
+++ server/routes/music/lpRoutes.js
@@ -3,20 +3,26 @@ import {
     createLP,
     getAllLPs,
     getLP,
-    likeLP
+    likeLP,
+    updateLP,
+    deleteLP
 } from "../../controllers/music/lp.js";
 import { isAuthenticated } from "../../middleware/authentication.js";
 
 const router = express.Router();
 
-/* CREATE route */
-router.post("/add", isAuthenticated, createLP) // Create an LP resource
+/** CREATE route */
+router.post("/create", isAuthenticated, createLP) // Create an LP resource
 
-/* READ route */
+/** READ route */
 router.get("/:artist/:title", isAuthenticated, getLP); // Get single LP
 router.get("/", isAuthenticated, getAllLPs); // Return each LP in db
 
-/* UPDATE route */
-router.patch("/:artist/:title/update", isAuthenticated, likeLP); // Like an LP
+/** UPDATE route */
+router.patch("/update/:artist/:title", isAuthenticated, likeLP); // Like an LP
+router.patch("/update/:id", isAuthenticated, updateLP); // Update LP details
+
+/** DELETE route */
+router.delete("/delete/:id", isAuthenticated, deleteLP); // Delete an LP
 
 export default router;
