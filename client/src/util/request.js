79bc1dc399c9b60321ffcee679e00caef6844e87
Keir Hewitt (macbook) <keir11@hotmail.com>
2023-01-13 13:37:27 +0000

    Revert: Previous version was actually latest.

--- client/src/util/request.js
+++ client/src/util/request.js
@@ -15,9 +15,35 @@ export const Post = async (url, data) => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-        body: JSON.stringify(data)
     }
 
+    const res = await fetch(url, payload);
+    const _json = res.json();
+    return _json;
+}
+
+/** DELETE */
+export const Delete = async(url, id) => {
+    const payload = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }    
+    }
+    const res = await fetch(`url/${id}`, payload);
+    const _json = res.json();
+    return _json;
+}
+
+/** PATCH */
+export const Patch = async (url, data) => {
+    const payload = {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
     const res = await fetch(url, payload);
     const _json = res.json();
     return _json;
