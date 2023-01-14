3d9e2bdc8060781832ab2bbf77572ee6a4655563
Keir Hewitt (macbook) <keir11@hotmail.com>
2023-01-14 10:40:44 +0000

    Update: Rolled back to initial commit for Front End

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,9 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import MainPage from "pages/mainPage";
-import RegisterPage from "pages/registerPage";
-import LoginPage from "pages/loginPage";
-import Navbar from "pages/nav";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-        </Routes>
-      </BrowserRouter>
+      
     </div>
   );
 }
