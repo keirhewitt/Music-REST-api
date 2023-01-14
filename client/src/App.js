ad48ac8273c687888ba8b1638d789eac31f54364
Keir Hewitt (macbook) <keir11@hotmail.com>
2023-01-14 14:45:04 +0000

    Update: Changing to simplified Front End

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,40 @@
-import './App.css';
+import React, { Component, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Button, Container, Card, Row } from 'react-bootstrap'
+
+import LP from 'components/LP';
 
 function App() {
+    
+  const [artist, setArtist] = useState('');
+  const [title, setTitle] = useState('');
+  const [release, setRelease] = useState('');
+  const [res, setRes] = useState({});
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/swordfishtrombone/api/v1/music/lp")
+    .then((response) => response.json())
+    .then((response) => {
+      setRes(response);
+      setError(null);
+    })
+    .catch(setError);
+  }, []);
+
+  if (error) return <p>An error occurred</p>
+
   return (
     <div className="App">
-      
+      {res.map(({artist, title, releaseDate}) => {
+        <LP 
+          artist={artist}
+          title={title}
+          releaseDate={releaseDate}
+        />
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
