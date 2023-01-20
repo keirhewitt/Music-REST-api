aace023ea1c89f4047b3343d13bb119bc36f8c24
Keir Hewitt (macbook) <keir11@hotmail.com>
2023-01-20 13:11:19 +0000

    Update: Now checking for api key in header

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -2,11 +2,11 @@ import User from "../models/user/User.js";
 
 export const isAuthenticated = async (req, res, next) => {
     try {
-        const { apiKey } = req.body;
+        const apiKey = req.header('apiKey');
         const user = await User.findOne({
             apikey: apiKey
         })
-        if (!apiKey) return res.status(403).json({ message: "Invalid api key."})
+        if (!apiKey) return res.status(403).json({ message: "No api key provided."})
         else {
             if (!user) return res.status(400).json({ message: "Invalid api key." });
             else {
