92a440ade676bf14e78b6c1471aac2918a4dffc1
Keir Hewitt <keir11@hotmail.com>
2023-03-25 16:39:01 +0000

    Update: Removes Table, Update and Delete functions

--- client/src/pages/LP/ListLP.jsx
+++ client/src/pages/LP/ListLP.jsx
@@ -1,109 +1,31 @@
 import React, { Component } from 'react';
 import ReactTable from 'react-table';
-import { lp_urls } from 'api/api';
 import styled from 'styled-components';
+import { lp_urls } from "../../api/api";
 
-const Wrapper = styled.div`
-    padding: 0 40px 40px 40px;
-`
+/* Import Buttons */
+import { DeleteLP } from "../../components/buttons/DeleteLP";
+import { UpdateLP } from "../../components/buttons/UpdateLP";
 
-const Update = styled.div`
-    color: #ef9b0f;
-    cursor: pointer;
-`
+import LPTable from "../../components/Table";
 
-const Delete = styled.div`
-    color: #ff0000;
-    cursor: pointer;
-`
 
 class AllLPs extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            lps: [],
-            columns: [],
             isLoading: false,
         }
     }
 
-    componentDidMount = async () => {
+    // Wait for page to load all LPs and then update state
+    componentDidMount = () => {
         this.setState({ isLoading: true });
-
-        await lp_urls.getAllLPs().then(lp => {
-            this.setState({
-                lp: lp.data,
-                isLoading: false,
-            });
-        })
     }
 
     render() {
-        const { lps, isLoading } = this.state;
-
-        const columns = [
-            {
-                Header: 'ID',
-                accessor: '_id',
-                filterable: true,
-            },
-            {
-                Header: 'Artist',
-                accessor: 'artist',
-                filterable: true,
-            },
-            {
-                Header: 'Title',
-                accessor: 'title',
-                filterable: true,
-            },
-            {
-                Header: 'Release Date',
-                accessor: 'release',
-                Cell: props => <span>{props.releaseDate}</span>,
-            },
-            {
-                Header: '',
-                accessor: '',
-                Cell: function(props) {
-                    return (
-                        <span>
-                            <DeleteLP id={props._id} />
-                        </span>
-                    )
-                },
-            },
-            {
-                Header: '',
-                accessor: '',
-                Cell: function(props) {
-                    return (
-                        <span>
-                            <UpdateLP id={props._id} />
-                        </span>
-                    )
-                },
-            },
-        ]
-
-        let showTable = true;
-        if (!lps.length) {
-            showTable = false;
-        }
-
         return (
-            <Wrapper>
-                {showTable && (
-                    <ReactTable
-                        data={lps}
-                        columns={columns}
-                        loading={isLoading}
-                        defaultPageSize={10}
-                        showPageSizeOptions={true}
-                        minRows={0}
-                    />
-                )}
-            </Wrapper>
+            <LPTable />
         )
     }
 }
