3fe470d75e7f245fd15c63e14a0a13c0d7be9aea
Keir Hewitt <keir11@hotmail.com>
2023-05-25 11:38:52 +0100

    Remove API logic, converts styling to TailwindCSS, converts to regular HTML table

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -2,132 +2,51 @@
 	Export a React-Table
 */
 
-import React, { Component, useEffect, useState } from 'react'
-import styled from 'styled-components'
-import { useTable } from 'react-table'
+import React from 'react';
 
-import { lp_urls } from "../api/api";
+/* IMPORT BUTTON COMPONENTS */
+import UpdateLP from './buttons/UpdateLP';
+import DeleteLP from './buttons/DeleteLP';
 
-const Wrapper = styled.div`
-  padding: 1rem;
-
-  table {
-    border-spacing: 0;
-    border: 1px solid black;
-
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
-`
+/* STYLES */
+const tdStyle = `p-3 border-t-[0.5px] overflow-hidden text-ellipsis`;
 
 function Table({ columns, data }) {
-  // Use the state and functions returned from useTable to build your UI
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({
-    columns,
-    data,
-  })
-
-  // Render the UI for your table
   return (
-  	<>
-	    <table {...getTableProps()}>
-	      <thead>
-	        {headerGroups.map(headerGroup => (
-	          <tr {...headerGroup.getHeaderGroupProps()}>
-	            {headerGroup.headers.map(column => (
-	              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-	            ))}
-	          </tr>
-	        ))}
-	      </thead>
-	      <tbody {...getTableBodyProps()}>
-	        {rows.map((row, i) => {
-	          prepareRow(row)
-	          return (
-	            <tr {...row.getRowProps()}>
-	              {row.cells.map(cell => {
-	                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-	              })}
-	            </tr>
-	          )
-	        })}
-	      </tbody>
-	    </table>
-    </>
+  	<table className='overflow-hidden border-collapse table-fixed rounded-md whitespace-nowrap w-full overflow-x-auto'>
+		<thead className='bg-[#ccc] text-[#222]'>
+			<tr>
+				<th className='p-3 w-2/6'>{columns[0].Header}</th>
+				<th className='p-3 w-2/5'>{columns[1].Header}</th>
+				<th className='p-3 w-1/6'>{columns[2].Header}</th>
+				<th className='p-3 w-1/12'>{columns[3].Header}</th>
+			</tr>
+		</thead>
+		<tbody>
+			{data.map((row) => (
+				<>
+					<tr className=' hover:bg-slate-200'>
+						<td className={`${tdStyle}`}>{row.artist}</td>
+						<td className={`${tdStyle}`}>{row.title}</td>
+						<td className={`${tdStyle}`}>{row.releaseDate}</td>
+						<td className={`${tdStyle}`}>
+							<span className='flex w-full justify-around'>
+								<UpdateLP id={row._id}/>
+								<DeleteLP id={row._id}/>
+							</span>
+						</td>
+					</tr>
+				</>
+			))}	
+		</tbody>
+	</table>
   )
 }
 
-export default function CreateTable() {
-
-	const [lpdata, setLPData] = useState()
-	const [payload, setPayload] = useState(null)
-	const [loading, setLoading] = useState(true)
-
-	useEffect(() => {
-		getLPData()
-	}, [])
-
-	const getLPData = async () => {
-		await lp_urls.getAllLPs(payload).then((res) => {
-			setLPData(res.data)
-			setLoading(false)
-		})
-	}
-
-    const columns = React.useMemo(
-		() => [
-		  {
-		    Header: 'ID',
-		    accessor: '_id',
-		  },
-		  {
-		  	Header: 'Artist',
-		  	accessor: 'artist',
-		  	filterable: true
-		  },
-		  {
-		  	Header: 'Title',
-		  	accessor: 'title',
-		  	filterable: true
-		  },
-		  {
-		  	Header: 'Release Date',
-		  	accessor: 'releaseDate',
-		  	filterable: true
-		  },
-		],
-		[]
-	)
-
-    if (loading) {
-    	return <div className="loading-div">Loading...</div>
-    }
+export default function CreateTable({ data, columns }) {
     return (
-		<Wrapper>
-			<Table columns={columns} data={lpdata} />
-		</Wrapper>
+		<div className='flex justify-center p-4'>
+			<Table columns={columns} data={data}/>
+		</div>
 	)
 }
