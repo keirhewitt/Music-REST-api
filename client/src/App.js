b1ca6ef16b0170e42bd3e32a0de85ec3e8296f5c
Keir Hewitt <keir11@hotmail.com>
2023-05-25 11:40:39 +0100

    Updates Router paths for new pages

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,22 @@ import UpdatesLP from 'pages/LP/UpdatingLP';
 import CreatesLP from 'pages/LP/CreatingLP';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MainPage from 'pages/LP/MainPage';
+import RandomLP from 'pages/LP/RandomLP';
 
 function App() {
     return (
         <Router>
             <Navbar />
             <Routes>
-                <Route path="/music/lp" element={<AllLPs />} />
+                <Route path="/" element={ <MainPage /> } />
+                <Route path="/music/lp" element={ <AllLPs /> } />
                 <Route
                     path="/music/lp/update/:id"
-                    element={<UpdatesLP />}
+                    element={ <UpdatesLP /> }
                 />
-                <Route path="/music/lp/create" element={<CreatesLP />}/>
-
+                <Route path="/music/lp/create" element={ <CreatesLP /> }/>
+                <Route path="/music/lp/random" element={ <RandomLP /> } />
             </Routes>
         </Router>
     )
