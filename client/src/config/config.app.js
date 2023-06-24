fe115c583ec8e3b05f9a1ce4a1936f7d1dc28af2
Keir Hewitt (macbook) <keir11@hotmail.com>
2023-06-24 11:08:24 +0100

    Creates config file for consolidating env data

---
+++ client/src/config/config.app.js
@@ -0,0 +1,10 @@
+export const config = {
+    "test": {
+        "header": {
+
+            "headers": { "apiKey": process.env.REACT_APP_API_KEY }
+        },
+        "body": {},
+        "Authorization": {}
+    }
+}
