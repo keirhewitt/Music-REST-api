049ac2c3e93e663dcab3b991d11f37c5f0b34da8
Keir Hewitt (macbook) <keir11@hotmail.com>
2023-07-09 18:51:05 +0100

    Adds Register functions

--- server/controllers/user/user.js
+++ server/controllers/user/user.js
@@ -1,5 +1,9 @@
 import User from "../../models/user/User.js";
 import { validatePasswordHash } from "./authentication.js";
+import { generateApiKey } from "generate-api-key";
+import { isValidEmail } from "./authentication.js";
+import bcrypt from "bcrypt";
+
 // import { Jwt } from "jsonwebtoken";
 
 /* USER LOGIN */
@@ -33,7 +37,35 @@ export const Logout = async (req, res) => {
 
 /* USER REGISTER */
 export const Register = async (req, res) => {
-    return res.status(200).json({
-        message: "Not implemented. Mock success."
-    });
+
+    try {
+        const { email, password } = req.body;
+        const {valid, reason, validators} = await isValidEmail(email);  // Validate email address
+
+        if (!valid) {
+            return res.status(400).json({
+                message: "Email address invalid.",
+                reason: validators[reason].reason
+            });
+        }
+        
+        if (User.findOne({ email: email})) return res.status(303).json({ error: "Email already registered in database." });
+
+        const salt = await bcrypt.genSalt();
+        const hashedPass = await bcrypt.hash(password, salt);
+
+        const newuser = new User({
+            email,
+            password: hashedPass,
+            apikey: generateApiKey.generateApiKey({ length: 16 })
+        });
+
+        await newuser.save();
+
+        /* Send user API key via email */
+        sendAPIKey(newuser.apikey, newuser.email);
+        res.status(201).json({ msg: "Your API Key is being sent to your email." });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 }
