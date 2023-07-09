e9794e616cc016eab4bbe303a556ecddd03e37a2
Keir Hewitt (macbook) <keir11@hotmail.com>
2023-07-09 18:51:55 +0100

    Adds Login, Register routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import AllLPs from './pages/LP/ListLP';
 import UpdatesLP from 'pages/LP/UpdatingLP';
 import CreatesLP from 'pages/LP/CreatingLP';
 
+import Login from 'pages/User/Login';
+import Register from 'pages/User/Register';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MainPage from 'pages/LP/MainPage';
 import RandomLP from 'pages/LP/RandomLP';
@@ -23,6 +26,10 @@ function App() {
                 />
                 <Route path="/music/lp/create" element={ <CreatesLP /> }/>
                 <Route path="/music/lp/random" element={ <RandomLP /> } />
+
+                <Route path="/user/login" element={ <Login /> } />
+                <Route path="/user/register" element={ <Register /> } />
+
             </Routes>
         </Router>
     )
