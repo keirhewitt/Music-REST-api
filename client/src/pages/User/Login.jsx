bc2ef6bd2e8674dc4581852882e805714e18759e
Keir Hewitt (macbook) <keir11@hotmail.com>
2023-07-09 18:53:47 +0100

    Adds Login, Register pages

--- client/src/pages/User/Login.jsx
+++ client/src/pages/User/Login.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
+const textBoxStyle = 
+`peer block min-h-[auto] w-full rounded border bg-transparent 
+px-3 py-[0.32rem] leading-[1.6] outline-none transition-all 
+duration-200 ease-linear focus:placeholder:opacity-100 
+peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 
+motion-reduce:transition-none dark:text-neutral-200 
+dark:placeholder:text-neutral-200 dark:peer-focus:text-primary 
+[&:not([data-te-input-placeholder-active])]:placeholder:opacity-0`
+const submitButtonStyle = 
+`rounded-sm bg-lime-500 hover:bg-lime-400 py-2 px-4 transition duration-100`
+const labelStyle = 
+`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] 
+truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all 
+duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] 
+peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] 
+peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none 
+dark:text-neutral-200 dark:peer-focus:text-primary`
+
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='w-5/6 flex m-auto h-screen'>
+      <form className='w-[300px] h-[300px] m-auto flex flex-col justify-around p-3 bg-dark rounded-lg' action='' method='POST'>
+        <div className='relative mb-3' data-te-input-wrapper-init>
+          <input className={textBoxStyle} type="text" id='emailInput' placeholder='Email'/>
+          <label for='emailInput' className={labelStyle}>Email</label>
+        </div>
+        <div className='relative mb-3' data-te-input-wrapper-init>
+          <input className={textBoxStyle} type='password' id='password' placeholder='Password'/>
+          <label for='password' className={labelStyle}>Password</label>
+        </div>
+        <input className={submitButtonStyle} type='submit' value='Login' />
+      </form>
+    </div>
   )
 }
 
--- client/src/pages/User/Register.jsx
+++ client/src/pages/User/Register.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const textBoxStyle = 
+`peer block min-h-[auto] w-full rounded border bg-transparent 
+px-3 py-[0.32rem] leading-[1.6] outline-none transition-all 
+duration-200 ease-linear focus:placeholder:opacity-100 
+peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 
+motion-reduce:transition-none dark:text-neutral-200 
+dark:placeholder:text-neutral-200 dark:peer-focus:text-primary 
+[&:not([data-te-input-placeholder-active])]:placeholder:opacity-0`
+const submitButtonStyle = 
+`rounded-sm bg-lime-400 hover:bg-lime-500 py-2 px-4 transition duration-100`
+const labelStyle = 
+`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] 
+truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all 
+duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] 
+peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] 
+peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none 
+dark:text-neutral-200 dark:peer-focus:text-primary`
 
 const Register = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const [errors, setErrors] = useState({})
+
+  const handleChange = (e) => {
+
+  }
+
   return (
-    <div>Register</div>
+    <div className='w-5/6 flex m-auto h-screen'>
+      <form className='w-[300px] h-[300px] m-auto flex flex-col justify-around p-3 bg-dark rounded-lg' action='' method='POST'>
+        <div className='relative mb-3' data-te-input-wrapper-init>
+          <input className={textBoxStyle} type="text" id='emailInput' placeholder='Email'/>
+          <label for='emailInput' className={labelStyle}>Email</label>
+        </div>
+        <div className='relative mb-3' data-te-input-wrapper-init>
+          <input className={textBoxStyle} type='password' id='password' placeholder='Password'/>
+          <label for='password' className={labelStyle}>Password</label>
+        </div>
+        <div className='relative mb-3' data-te-input-wrapper-init>
+          <input className={textBoxStyle} type='password' id='confirm-password' placeholder='Re-enter assword'/>
+          <label for='confirm-password' className={labelStyle}>Confirm Password</label>
+        </div>
+        <input className={submitButtonStyle} type='submit' value='Register' />
+      </form>
+    </div>
   )
 }
 
