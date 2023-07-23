27a76c6d5f2e056f40e45b029a34ccf4aac968e0
Keir Hewitt (macbook) <keir11@hotmail.com>
2023-07-23 12:01:26 +0100

    Update: Changed Register route, created Login route

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -8,6 +8,22 @@ const api = axios.create({
 
 /* Temp payload for testing purposes */
 const test_payload = config.test.header;
+const user_test_payload = config.test.body;
+
+let data = JSON.stringify({
+    "email": "keir11@hotmail.com",
+    "password": "test"
+});
+
+let conf = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'http://localhost:8000/swordfishtrombone/api/v1/user/register',
+    headers: { 
+        'Content-Type': 'application/json'
+    },
+    data : data
+};
 
 /* LP */
 const createLP = payload => api.post(`/music/lp/create`, test_payload);
@@ -17,6 +33,30 @@ const deleteLP = id => api.delete(`/music/lp/delete/${id}`, test_payload);
 
 /* User */
 const createUser = payload => api.post(`/user/create`, test_payload);
+const loginUser = (payload, id) => api.post(`/user/login/${id}`, test_payload);
+const logoutUser = payload => api.post(`/user/logout`, test_payload);
+const registerUser = (email, password) => api.post(`/user/register`, {
+    headers: { 
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        email: email,
+        password: password
+    })
+})
+
+
+
+async function makeRequest() {
+    try {
+      const response = await axios.request(conf);
+      console.log(JSON.stringify(response.data));
+    }
+    catch (error) {
+      console.log(error);
+    }
+}
+
 
 export const LP__ROUTES = {
     createLP,
@@ -26,6 +66,9 @@ export const LP__ROUTES = {
 };
 
 export const USER__ROUTES = {
-    createUser
+    createUser,
+    loginUser,
+    logoutUser,
+    registerUser
 };
 
