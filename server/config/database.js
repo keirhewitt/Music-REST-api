ce27f615feabdb6d1fd728cfdc68bd9ea6595a03
Keir Hewitt (macbook) <keir11@hotmail.com>
2023-07-23 11:53:15 +0100

    Update: Turned database into service @ config/ dir

---
+++ server/config/database.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env"});
+
+export function connect() {
+  // Connecting to the database
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => {
+      console.log('--Connected to database');
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
+    });
+}
