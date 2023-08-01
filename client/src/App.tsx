d0bf67f30de4be8bb8dd09f859d8c2c2baef21bb
Keir Hewitt <keir11@hotmail.com>
2023-08-01 21:24:08 +0100

    Small formatting changes...

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Navbar } from './components';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navbar } from "./components";
 
-import AllLPs from './pages/LP/ListLP';
-import UpdatesLP from './pages/LP/UpdatingLP';
-import CreatesLP from './pages/LP/CreatingLP';
+import AllLPs from "./pages/LP/ListLP";
+import UpdatesLP from "./pages/LP/UpdatingLP";
+import CreatesLP from "./pages/LP/CreatingLP";
 
-import Login from './pages/User/Login';
-import Register from './pages/User/Register';
+import Login from "./pages/User/Login";
+import Register from "./pages/User/Register";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MainPage from './pages/LP/MainPage';
-import RandomLP from './pages/LP/RandomLP';
+import "bootstrap/dist/css/bootstrap.min.css";
+import MainPage from "./pages/LP/MainPage";
+import RandomLP from "./pages/LP/RandomLP";
 
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={ <MainPage /> } />
-                <Route path="/music/lp" element={ <AllLPs /> } />
-                <Route
-                    path="/music/lp/update/:id"
-                    element={ <UpdatesLP /> }
-                />
-                <Route path="/music/lp/create" element={ <CreatesLP /> }/>
-                <Route path="/music/lp/random" element={ <RandomLP /> } />
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/music/lp" element={<AllLPs />} />
+        <Route path="/music/lp/update/:id" element={<UpdatesLP />} />
+        <Route path="/music/lp/create" element={<CreatesLP />} />
+        <Route path="/music/lp/random" element={<RandomLP />} />
 
-                <Route path="/user/login" element={ <Login /> } />
-                <Route path="/user/register" element={ <Register /> } />
-
-            </Routes>
-        </Router>
-    )
+        <Route path="/user/login" element={<Login />} />
+        <Route path="/user/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
