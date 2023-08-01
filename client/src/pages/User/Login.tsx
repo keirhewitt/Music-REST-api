fd71c54e3114e2ebfd35120e2f75fe3faedcceef
Keir Hewitt (macbook) <keir11@hotmail.com>
2023-08-01 16:28:03 +0100

    Feature: Implements form validation, changes style to bootstrap 4

--- client/src/pages/User/Login.tsx
+++ client/src/pages/User/Login.tsx
@@ -1,41 +1,52 @@
 import React from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { yupResolver } from "@hookform/resolvers/yup";
 
-const textBoxStyle = 
-`peer block min-h-[auto] w-full rounded border bg-transparent 
-px-3 py-[0.32rem] leading-[1.6] outline-none transition-all 
-duration-200 ease-linear focus:placeholder:opacity-100 
-peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 
-motion-reduce:transition-none dark:text-neutral-200 
-dark:placeholder:text-neutral-200 dark:peer-focus:text-primary 
-[&:not([data-te-input-placeholder-active])]:placeholder:opacity-0`
-const submitButtonStyle = 
-`rounded-sm bg-lime-500 hover:bg-lime-400 py-2 px-4 transition duration-100`
-const labelStyle = 
-`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] 
-truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all 
-duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] 
-peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] 
-peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none 
-dark:text-neutral-200 dark:peer-focus:text-primary`
+/* Login form validation schema */
+import { loginSchema } from '../../config/form.validation';
 
 const Login = () => {
 
-  const handleSubmit = (e: Event) => {
+  const { register, handleSubmit, reset, formState: {errors}, } = useForm({ 
+    resolver: yupResolver(loginSchema)
+  });
 
+  /* TODO: Implement Login functionality */
+  async function onSubmit (data: any) {
+    console.log(JSON.stringify(data));
   }
 
   return (
     <div className='w-5/6 flex m-auto h-screen'>
-      <form className='w-[300px] h-[300px] m-auto flex flex-col justify-around p-3 bg-dark rounded-lg' action='' method='POST'>
-        <div className='relative mb-3' data-te-input-wrapper-init>
-          <input className={textBoxStyle} type="text" id='emailInput' placeholder='Email'/>
-          <label htmlFor='emailInput' className={labelStyle}>Email</label>
+      <form 
+        onSubmit={handleSubmit(onSubmit)} 
+        className='w-[300px] m-auto flex flex-col justify-around px-3 py-6 bg-slate-50 rounded-lg'>
+        
+        {/* Email Input */}
+        <div className='relative mb-1'>
+          <label htmlFor='emailInput'>Email</label>
+          <input
+            type='text' 
+            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+            {...register("email")}
+            aria-invalid={errors.email ? "true" : "false"}/>
+            <div className="invalid-feedback">{errors.email?.message}</div>
         </div>
-        <div className='relative mb-3' data-te-input-wrapper-init>
-          <input className={textBoxStyle} type='password' id='password' placeholder='Password'/>
-          <label htmlFor='password' className={labelStyle}>Password</label>
+
+        {/* Password Input */}
+        <div className='relative mb-1'>
+          <label htmlFor='password'>Password</label>
+          <input 
+            type='password' 
+            className={`form-control ${errors.password ? 'is-invalid' : ''}`}     
+            {...register("password")} 
+            id='password'/>
+            <div className="invalid-feedback">{errors.password?.message}</div>
         </div>
-        <input className={submitButtonStyle} type='submit' value='Login' />
+
+        {/* Submit Button */}
+        <input className='btn btn-primary mt-3' type='submit' value='Login' />
+
       </form>
     </div>
   )
--- client/src/pages/User/Register.tsx
+++ client/src/pages/User/Register.tsx
@@ -1,36 +1,23 @@
 import React, { useState } from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
+import { yupResolver } from "@hookform/resolvers/yup";
 import swal from 'sweetalert'
 import axios from 'axios'
+
+/* Register form validation schema */
+import { registerSchema } from '../../config/form.validation'
 
 type Credentials = {
   email: string,
   password: string
 }
 
-/** Tailwind styles */
-const textBoxStyle = 
-`peer block min-h-[auto] w-full rounded border bg-transparent 
-px-3 py-[0.32rem] leading-[1.6] outline-none transition-all 
-duration-200 ease-linear focus:placeholder:opacity-100 
-peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 
-motion-reduce:transition-none dark:text-neutral-200 
-dark:placeholder:text-neutral-200 dark:peer-focus:text-primary 
-[&:not([data-te-input-placeholder-active])]:placeholder:opacity-0`
-const submitButtonStyle = 
-`rounded-sm bg-lime-400 hover:bg-lime-500 py-2 px-4 transition duration-100`
-const labelStyle = 
-`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] 
-truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all 
-duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] 
-peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] 
-peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none 
-dark:text-neutral-200 dark:peer-focus:text-primary`
-
 /** Register a User */
 const Register = () => {
 
-  const { register, handleSubmit, formState: {errors}, } = useForm()
+  const { register, handleSubmit, reset, formState: {errors}, } = useForm({ 
+    resolver: yupResolver(registerSchema)
+  });
 
   /* JSONify credentials, create POST request and send to url */
   async function registerUser(creds: Credentials) {
@@ -71,36 +58,44 @@ const Register = () => {
     <div className='w-5/6 flex m-auto h-screen'>
       <form 
         onSubmit={handleSubmit(onSubmit)} 
-        className='w-[300px] h-[300px] m-auto flex flex-col justify-around p-3 bg-dark rounded-lg'>
-        <div className='relative mb-3' data-te-input-wrapper-init>
+        className='w-[500px] m-auto flex flex-col justify-around px-4 py-8 bg-slate-50 rounded-lg'>
+        
+        {/* Email Input */}
+        <div className='relative mb-1'>
+          <label htmlFor='emailInput'>Email</label>
           <input
             type='text' 
-            className={textBoxStyle} 
-            {...register("email", { required: true })}
+            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+            {...register("email")}
             aria-invalid={errors.email ? "true" : "false"}/>
-          <label 
-            htmlFor='emailInput' 
-            className={labelStyle}>Email</label>
+            <div className="invalid-feedback">{errors.email?.message}</div>
         </div>
-        <div className='relative mb-3' data-te-input-wrapper-init>
+
+        {/* Password Input */}
+        <div className='relative mb-1'>
+          <label htmlFor='password'>Password</label>
           <input 
             type='password' 
-            className={textBoxStyle} 
-            {...register("password", { required: true })} 
+            className={`form-control ${errors.password ? 'is-invalid' : ''}`}     
+            {...register("password")} 
             id='password'/>
-          <label htmlFor='password' className={labelStyle}>Password</label>
+            <div className="invalid-feedback">{errors.password?.message}</div>
         </div>
-        <div className='relative mb-3' data-te-input-wrapper-init>
+
+        {/* Confirm Password Input */}
+        <div className='relative mb-1'>
+          <label htmlFor='confirm-password'>Confirm Password</label>
           <input 
             type='password' 
-            className={textBoxStyle}
-            {...register("confirmpassword", { required: true })}  
+            className={`form-control ${errors.confirmpassword ? 'is-invalid' : ''}`}
+            {...register("confirmpassword")}  
             id='confirm-password'/>
-          <label 
-            htmlFor='confirm-password' 
-            className={labelStyle}>Confirm Password</label>
+            <div className="invalid-feedback">{errors.confirmpassword?.message}</div>
         </div>
-        <input className={submitButtonStyle} type='submit' value='Register' />
+
+        {/* Submit Button */}
+        <input className='btn btn-primary mt-3' type='submit' value='Register' />
+
       </form>
     </div>
   )
